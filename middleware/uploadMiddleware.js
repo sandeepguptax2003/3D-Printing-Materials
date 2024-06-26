4c08825310daa813eed8d920517a4803f1b26669
sandeep <sandeepguptax2003@gmail.com>
2024-06-26 11:14:37 +0530

    Added middleware

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const storage = multer.memoryStorage();
+
+
+const fileFilter = (req, file, cb) => {
+  // Accept image files only
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Not an image! Please upload an image.'), false);
+  }
+};
+
+// Configure multer
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB file size limit
+  },
+});
+
+const uploadMiddleware = (req, res, next) => {
+  upload.single('image')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(400).json({ message: 'File size is too large. Max limit is 5MB' });
+      }
+      return res.status(400).json({ message: err.message });
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      return res.status(400).json({ message: err.message });
+    }
+    // Everything went fine.
+    next();
+  });
+};
+
+module.exports = uploadMiddleware;
